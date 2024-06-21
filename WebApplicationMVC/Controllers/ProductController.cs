0aa3a0c61fc3b9fd9ea2921cbd4beb990e89c9ea
Franklin Caamaño <franklinstone98@gmail.com>
2024-06-21 14:16:51 -0400

    Add project files.

---
+++ WebApplicationMVC/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationMVC.Models.Products;
+using WebApplicationMVC.Models.Servicios;
+
+namespace WebApplicationMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Constructor: Product
+        public ProductController()
+        {
+
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            var model = ProductoServicio.GetProductos();
+            return View(model);
+        }
+        public ActionResult Create(Product producto)
+        {
+             
+            return View(producto);
+        }
+        
+    }
+}
---
+++ WebApplicationMVC/Models/Servicios/ProductoServicio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplicationMVC.Models.Products;
+
+namespace WebApplicationMVC.Models.Servicios
+{
+    public static class ProductoServicio
+    {
+        public static List<Product> GetProductos()
+        {
+            List<Product> products = new List<Product>();
+            products.Add(new Product() { Id = 1, Name = "Básico", Price = Convert.ToDecimal("23500.33") });
+            products.Add(new Product() { Id = 2, Name = "Especial", Price = Convert.ToDecimal("42500.89") });
+            products.Add(new Product() { Id = 3, Name = "Superior", Price = Convert.ToDecimal("56800.44") });
+
+            return products;
+        }
+    }
+}
